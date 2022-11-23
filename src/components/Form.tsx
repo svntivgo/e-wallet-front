444cccf55553558d7cbcaa311496649823dedd5c
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-23 00:32:57 -0500

    loans form

---
+++ src/components/Form.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ButtonBig from './ButtonBig';
+
+interface Props {
+  inputs: { logo: string; name: string; error: string }[];
+  button: string;
+}
+
+const Form = ({ inputs, button }: Props) => {
+  return (
+    <View style={styles.formContainer}>
+      {inputs.map((input: any) => {
+        return (
+          <View key={input.name} style={styles.formItem}>
+            <Icon style={styles.logo} name={input.logo} size={25} />
+            <View style={styles.input}>
+              <TextInput style={styles.textInput} placeholder={input.name} />
+              <View style={styles.errorContainer}>
+                <Text style={styles.error}>{input.error}</Text>
+              </View>
+            </View>
+          </View>
+        );
+      })}
+      <ButtonBig text={button} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formContainer: {
+    paddingHorizontal: 10,
+  },
+  formItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
+  logo: {
+    flex: 1,
+    paddingBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    flex: 4,
+  },
+  textInput: {
+    backgroundColor: '#eaeaea',
+    borderBottomWidth: 1,
+    borderBottomColor: '#777777',
+    padding: 10,
+    color: 'black',
+    borderRadius: 5,
+  },
+  errorContainer: {
+    height: 20,
+  },
+  error: {
+    display: 'none',
+    color: 'red',
+    marginLeft: 10,
+  },
+});
+
+export default Form;
--- src/screens/Loans.tsx
+++ src/screens/Loans.tsx
@@ -1,75 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import ButtonBig from '../components/ButtonBig';
+import { StyleSheet, View } from 'react-native';
+import AccountBalance from '../components/AccountBalance';
+import Form from '../components/Form';
 
-const Loans = () => (
-  <View style={styles.container}>
-    <View style={styles.formItem}>
-      <Text style={styles.logo}>Logo</Text>
-      <View style={styles.input}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Soy un placeholder jeje"
-        />
-        <View style={styles.errorContainer}>
-          <Text style={styles.error}>Error, oh no!</Text>
-        </View>
-      </View>
+const Loans = () => {
+  const inputs: { logo: string; name: string; error: string }[] = [
+    { logo: 'cash-multiple', name: 'Amount', error: '' },
+    { logo: 'message-text-outline', name: 'Reason', error: '' },
+  ];
+
+  return (
+    <View style={styles.Container}>
+      <AccountBalance amount={50000000} text={'Available to loan'} />
+      <Form inputs={inputs} button={'Apply for loan'} />
     </View>
-    <View style={styles.formItem}>
-      <Text style={styles.logo}>Logo</Text>
-      <View style={styles.input}>
-        <TextInput style={styles.textInput} placeholder="Amount" />
-        <View style={styles.errorContainer}>
-          <Text style={styles.error}>Error, oh no!</Text>
-        </View>
-      </View>
-    </View>
-    <View style={styles.formItem}>
-      <Text style={styles.logo}>Logo</Text>
-      <View style={styles.input}>
-        <TextInput style={styles.textInput} placeholder="Reason" />
-        <View style={styles.errorContainer}>
-          <Text style={styles.error}>Error, oh no!</Text>
-        </View>
-      </View>
-    </View>
-    <ButtonBig text={'Apply for loan'} />
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    paddingHorizontal: 10,
-  },
-  formItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 5,
-  },
-  logo: {
-    flex: 1,
-    paddingBottom: 20,
-  },
-  input: {
-    flex: 4,
-  },
-  textInput: {
-    backgroundColor: '#eaeaea',
-    borderBottomWidth: 1,
-    borderBottomColor: '#777777',
-    padding: 10,
-    color: 'black',
-    borderRadius: 5,
-  },
-  errorContainer: {
-    height: 20,
-  },
-  error: {
-    display: 'none',
-    color: 'red',
-    marginLeft: 10,
   },
 });
 
